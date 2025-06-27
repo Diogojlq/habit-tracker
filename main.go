80cf9b06054dd23af973a4874260e0c1cb43aabf
Diogo <fmlsp2015@gmail.comnasdas>
2025-06-27 18:10:27 -0300

    fix: resolving windows error message when running backend

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
     RegisterRoutes()
     fmt.Println("Servidor rodando em http://localhost:8080")
-    http.ListenAndServe(":8080", nil)
+    http.ListenAndServe("127.0.0.1:8080", nil)
 }
