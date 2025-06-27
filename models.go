b1875c619a0251e5f81b6f51d30c7e5b4ec30f3e
Diogo <fmlsp2015@gmail.comnasdas>
2025-06-27 19:03:31 -0300

    chore: basic user registration API

--- models.go
+++ models.go
@@ -4,3 +4,9 @@ type Habit struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
+
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
