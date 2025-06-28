62b8c1e6cdc3ce69812d9f22e4429f1e2cc9ada4
Diogo <fmlsp2015@gmail.comnasdas>
2025-06-27 22:56:13 -0300

    feat/chore: user registration connection with DB and CORS configs

--- main.go
+++ main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-    "gorm.io/gorm"
-    "gorm.io/driver/postgres"
-    "log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
+
+func enableCORS(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+        next.ServeHTTP(w, r)
+    })
+}
 
 var db *gorm.DB
 
 func initDB() {
     var err error
-    dsn := "host=localhost user=postgres password=password dbname=habittracker port=5432 sslmode=disable"
+    dsn := os.Getenv("DATABASE_URL")
     db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
     if err != nil {
         log.Fatal("failed to connect database")
@@ -21,8 +37,9 @@ func initDB() {
 }
 
 func main() {
+    godotenv.Load()
     initDB()
     RegisterRoutes()
     fmt.Println("Servidor rodando em http://localhost:8080") 
-    http.ListenAndServe("127.0.0.1:8080", nil) // change to ":8080" when pushing to prod
+    http.ListenAndServe("127.0.0.1:8080", enableCORS(http.DefaultServeMux)) // change to ":8080" when pushing to prod
 }
