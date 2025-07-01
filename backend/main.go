0f615d70dcc535e76d888c31c54c4e7655e21536
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-01 18:21:54 -0300

    refactor: move backend files into backend folder for better project organization

--- backend/main.go
+++ backend/main.go
@@ -11,6 +11,7 @@ import (
 	"gorm.io/gorm"
 )
 
+
 func enableCORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Access-Control-Allow-Origin", "*")
