52c2495e40fb33617615a3508eb0f65cc3789534
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-02 18:21:03 -0300

    feat: add gradient backgroud to homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,9 +2,8 @@ import AuthButtons from "./components/AuthButtons";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center px-6 py-12 space-y-16 min-h-screen bg-base-200 text-base-content">
-      
-      {/* Hero */}
+    <main className="flex flex-col items-center justify-center px-6 py-12 space-y-16 min-h-screen bg-gradient-to-r from-blue-900 via-blue-800 to-cyan-700  text-white text-base-content"
+    >
       <section className="text-center space-y-4 max-w-2xl">
         <h1 className="text-4xl md:text-5xl font-bold">
           Develop a better life with Habit Tracker
@@ -16,7 +15,6 @@ export default function Home() {
         <AuthButtons />
       </section>
 
-      {/* Preview visual */}
       <section className="w-full max-w-4xl">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {[1, 2, 3].map((_, i) => (
@@ -24,7 +22,7 @@ export default function Home() {
               key={i}
               className="card bg-base-100 shadow-xl rounded-2xl p-6"
             >
-              <h3 className="text-xl font-semibold mb-2">Hábito #{i + 1}</h3>
+              <h3 className="text-xl font-semibold mb-2">Habit #{i + 1}</h3>
               <p className="text-gray-600 dark:text-gray-400 text-sm">
                 Mark as done, see progress, and keep the momentum going.
               </p>
@@ -33,7 +31,6 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Features */}
       <section className="text-center space-y-4 max-w-2xl">
         <h2 className="text-2xl font-semibold">Made for your everyday life</h2>
           <ul className="list-disc list-inside text-gray-700 dark:text-gray-400 space-y-2">
