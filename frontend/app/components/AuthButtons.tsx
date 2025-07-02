50b63de77502c10207fde128072086ec778d4581
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-02 17:41:20 -0300

    feat(frontend): add redirect to register button

--- frontend/app/components/AuthButtons.tsx
+++ frontend/app/components/AuthButtons.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function AuthButtons() {
   return (
     <div className="flex space-x-4 justify-center">
-      <button className="btn btn-primary">Register</button>
+      <Link href="/register" passHref>
+        <button className="btn btn-primary">Register</button>
+      </Link>
       <button className="btn btn-outline btn-primary">Login</button>
     </div>
   );
