d9623c306f5d91551f07b7e2ab73c4e5a5843fd5
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-02 18:33:01 -0300

    feat(frontend): add helthy habits image to homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,31 @@
+import Image from "next/image";
 import AuthButtons from "./components/AuthButtons";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center px-6 py-12 space-y-16 min-h-screen bg-gradient-to-r from-blue-900 via-blue-800 to-cyan-700  text-white text-base-content"
-    >
-      <section className="text-center space-y-4 max-w-2xl">
-        <h1 className="text-4xl md:text-5xl font-bold">
-          Develop a better life with Habit Tracker
-        </h1>
-        <p className="text-gray-600 dark:text-gray-300 text-lg">
-          Track, improve and conquer your routine with a simple and powerful app.
-        </p>
-        <button className="btn btn-primary btn-lg mt-4">Start now</button>
-        <AuthButtons />
+    <main className="flex flex-col items-center justify-center px-6 py-12 space-y-16 min-h-screen bg-gradient-to-r from-blue-900 via-blue-800 to-cyan-700 text-white">
+      <section className="grid grid-cols-1 md:grid-cols-2 items-center gap-12 w-full max-w-6xl">
+        <div className="space-y-6">
+          <h1 className="text-4xl md:text-5xl font-bold leading-tight">
+            Develop a better life with Habit Tracker
+          </h1>
+          <p className="text-lg text-white/80">
+            Track, improve and conquer your routine with a simple and powerful app.
+          </p>
+          <button className="btn btn-primary btn-lg">Start now</button>
+          <AuthButtons />
+        </div>
+
+        <div className="flex justify-center">
+          <Image
+            src="/healthy-habits-illustration.png"
+            alt="Healthy habits illustration"
+            width={400}
+            height={400}
+            className="rounded-xl shadow-xl"
+            priority
+            ></Image>
+        </div>
       </section>
 
       <section className="w-full max-w-4xl">
