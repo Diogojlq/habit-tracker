3f77b919e6e3fb9c9d0487ee94262fca669678fa
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-02 17:38:45 -0300

    feat(frontend): create login and register buttons component

---
+++ frontend/app/components/AuthButtons.tsx
@@ -0,0 +1,8 @@
+export default function AuthButtons() {
+  return (
+    <div className="flex space-x-4 justify-center">
+      <button className="btn btn-primary">Register</button>
+      <button className="btn btn-outline btn-primary">Login</button>
+    </div>
+  );
+}
