fc57b18e9574db7e647ed84e70723b31a4813a97
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-03 21:01:55 -0300

    feat(backend): add login logic and JWT authentication endpoint

--- backend/models.go
+++ backend/models.go
@@ -13,3 +13,12 @@ type User struct {
     Email    string
     Password string
 }
+
+type LoginRequest struct {
+    Email    string `json:"email"`
+    Password string `json:"password"`
+}
+
+type LoginResponse struct {
+    Token string `json:"token"`
+}
--- backend/utils/security.go
+++ backend/utils/security.go
@@ -2,6 +2,9 @@ package utils
 
 import (
 	"golang.org/x/crypto/bcrypt"
+	"time"
+	"os"
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func GenerateHashPassword(password string) (string, error) {
@@ -12,4 +15,13 @@ func GenerateHashPassword(password string) (string, error) {
 func CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+func GenerateJWT(userID uint) (string, error) {
+	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	return token.SignedString(jwtSecret)
 }
