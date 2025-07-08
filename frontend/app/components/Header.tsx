b084c11d7e72e391c8fc968a14547e2554415389
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-08 19:34:37 -0300

    feat(Header): change background color

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="bg-white">
+        <header className="bg-white dark:bg-gray-900" >
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
             <a className="block text-teal-600" href="#">
             <span className="sr-only">Home</span>
