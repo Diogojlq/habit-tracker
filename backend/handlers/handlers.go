568e03ed40820ccb50adf1fbe917f21fbc689e95
Diogo <fmlsp2015@gmail.comnasdas>
2025-07-28 20:11:05 -0300

    fix(handlers): fixing DBInterface config

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -9,15 +9,17 @@ import (
 	"gorm.io/gorm"
 )
 
+type DBInterface interface {
+	Create(value interface{}) *gorm.DB
+	Where(query interface{}, args ...interface{}) *gorm.DB
+	First(dest interface{}, conds ...interface{}) *gorm.DB
+}
+
 type App struct {
-	DB *gorm.DB
+	DB DBInterface
 }
 
 func (app *App) CreateHabitHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
 	var habit models.Habit
 	err := json.NewDecoder(r.Body).Decode(&habit)
 	if err != nil {
