c16df9f1ed65dd60fcab11edb6534afb1184785c
Diogo <fmlsp2015@gmail.com>
2025-12-14 11:14:33 -0300

    fix main.go

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/Diogojlq/habit-tracker/backend/database"
@@ -11,22 +12,29 @@ import (
 )
 
 func enableCORS(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        if r.Method == "OPTIONS" {
-            w.WriteHeader(http.StatusOK)
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-    godotenv.Load()
-    database.Init()
-    router.RegisterRoutes(&handlers.App{DB: database.DB})
-    fmt.Println("Server running: http://localhost:8080") 
-    http.ListenAndServe("127.0.0.1:8080", enableCORS(http.DefaultServeMux)) // change to ":8080" when pushing to prod
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Println(".en not ofund.")
+	}
+
+	dbInstance := database.Init()
+
+	router.RegisterRoutes(&handlers.App{DB: dbInstance})
+
+	fmt.Println("Server running: http://localhost:8080")
+
+	log.Fatal(http.ListenAndServe(":8080", enableCORS(http.DefaultServeMux)))
 }
